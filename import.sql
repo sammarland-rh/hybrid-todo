91d5c78c568417ed28572d8984966689ede3aa1c
Sam Marland <smarland@redhat.com>
2020-04-24 11:29:16 +0100

    More relevant data in sql create for demo

--- import.sql
+++ import.sql
@@ -4,12 +4,8 @@ CREATE TABLE todo (
     completed         boolean
 );
 INSERT INTO todo(title, completed) VALUES ('Hello World', true );
-INSERT INTO todo(title, completed) VALUES ('Introduction to Quarkus', true);
-INSERT INTO todo(title, completed) VALUES ('Hibernate with Panache', false );
-INSERT INTO todo(title, completed) VALUES ('Visit Quarkus web site', false );
-INSERT INTO todo(title, completed) VALUES ('Star Quarkus project', false );
-INSERT INTO todo(title, completed) VALUES ('Star Quarkus project 2', false);
-INSERT INTO todo(title, completed) VALUES ('Star Quarkus project 3', false);
-INSERT INTO todo(title, completed) VALUES ('Star Quarkus project 4', false);
-INSERT INTO todo(title, completed) VALUES ('Star Quarkus project 5', false);
+INSERT INTO todo(title, completed) VALUES ('Build Todo app', true );
+INSERT INTO todo(title, completed) VALUES ('Move apps around', false);
+INSERT INTO todo(title, completed) VALUES ('Move to OpenShift on Google Cloud', false );
+INSERT INTO todo(title, completed) VALUES ('Move to AKS', false );
 
